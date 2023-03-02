772a1762a52ae9a048a36eb639051692ec1003e9
Indrajit <indrajitghosh912@gmail.com>
2023-03-02 08:04:01 +0530

    this file has been renamed from 'main.py' to 'old_main.py'

---
+++ old_main.py
@@ -0,0 +1,151 @@
+# WashingMachineSlots - A python script to create a .tex file which can generate 
+# a washing machine slots table.
+#
+# Author: Indrajit Ghosh
+#
+# Created On: Jan 23, 2022
+# Modified On: Dec 19, 2022
+#
+
+INDRAJIT = r"\textit{Indrajit (RF-8)}"
+INDRAJITS_CHOICE = 6 # Sunday
+
+SNEHA = r"\textit{Sneha (RF-7)}"
+
+
+from datetime import datetime, timedelta
+from wash_tex_templates import FRONT, BACK, DIAGBOX_STY
+from pathlib import Path
+import os, sys
+
+HOME = Path.home()
+DESKTOP = HOME / 'Desktop/'
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def iter_days(year, month):
+    dt = datetime(year, month, 1)
+    while dt.year == year and dt.month == month:
+        yield dt
+        dt += timedelta(days=1)
+
+
+def get_table_text(year, month):
+
+    s = ''
+
+    for dt in iter_days(year, month):
+        date = dt.strftime(f'%b %-d, {year}')
+
+        # Monday = 0, ..., Sunday = 6
+        if dt.weekday() == INDRAJITS_CHOICE:
+            # Indrajit's slot
+            text = date + "&" + INDRAJIT +  "&" + "" + r" & & \\" + "\n" + r"\hline" + "\n"
+
+        else:
+            text = date + r" & & & & \\" + "\n" + r"\hline" + "\n"
+            
+        s += text
+
+    return s
+
+
+def create_slots_tex(year, month, dir, filename):
+
+    with open(dir / filename, 'w') as f:
+        f.write(FRONT)
+
+        f.write(get_table_text(year, month))
+
+        f.write(BACK)
+
+
+def create_diagbox_sty_file(dir:Path):
+    """
+    This function writes `DIAGBOX_STY` into a file `dir/diagbox.sty`
+
+    Parameter:
+    ----------
+        `dir`: Path; the directory into which `diagbox.sty` will be created
+    """
+    with open(dir / 'diagbox.sty', 'w') as f:
+        f.write(DIAGBOX_STY)
+
+
+def generate_slots(year:int, month:int):
+
+    tex_filename = "wash_slot_" + datetime(year, month, 1).strftime('%b_%y') + ".tex"
+
+    output_dir = DESKTOP / f"wash_slots_{datetime(year, month, 1).strftime('%b_%y')}"
+    try:
+        os.makedirs(output_dir)
+        print("\n - TeX directory created.")
+
+        create_slots_tex(year, month, output_dir, tex_filename)
+        print(" - Main tex file has been written.")
+
+        create_diagbox_sty_file(dir=output_dir) # creating `diagbox.sty`
+
+        os.chdir(output_dir)
+        os.system(f"pdflatex {tex_filename} > {os.devnull}")
+        print(f"\n - The `pdf` has been created inside: ```{output_dir}```\n")
+
+    except FileExistsError:
+        print(f"There is already a directory with the name ``{output_dir.name}``. Delete that directory first and try again later!")
+
+
+def main():
+
+    y = None
+    m = None
+
+    if len(sys.argv) < 2:
+        m = int(input("Enter the month code (e.g. 1 for Jan): "))
+
+    elif len(sys.argv) == 2:
+        m = int(sys.argv[1])
+
+    else:
+        if '-m' in sys.argv:
+            m = int(sys.argv[sys.argv.index('-m') + 1])
+        if '-y' in sys.argv:
+            y = int(sys.argv[sys.argv.index('-y') + 1])
+
+    # Check if month is given or not. If not print USAGE
+    USAGE = f"""
+    WashingMachineSlots 
+    ---------------------
+    Author: Indrajit Ghosh
+    Created On: Jan 23, 2022
+    Modified On: Dec 19, 2022
+
+    Usages:
+        python3 main.py
+        python3 main.py <month>
+        python3 main.py -m <month> -y <year>
+    
+    Examples:
+        Following cmd will generate slots for August(8) for the current year
+            python3 main.py 8 
+
+        Generate slots for specific month November and year 2025
+            python3 main.py -m 11 -y 2025
+    ...........................................................................
+    ...........................................................................
+
+    """
+
+    if m is None:
+        print(USAGE)
+        sys.exit()
+    else:
+        y = datetime.now().year if y is None else y
+        
+        clear_screen()
+        print(USAGE)
+        generate_slots(year=y, month=m)
+
+
+if __name__ == '__main__':
+    main()
